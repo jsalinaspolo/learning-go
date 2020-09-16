2df4cd03fa631a54a2c6dae352d965a19847c4a0
Javier Salinas <jsalinaspolo@gmail.com>
2020-09-16 19:08:46 +0200

    Add spanish language

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,14 +4,18 @@ import "fmt"
 
 const prefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		return prefix + "world"
-	} 
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
 
 	return prefix + name
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello("world", ""))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
 		if got != want {
@@ -12,17 +11,23 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Javier")
+		got := Hello("Javier", "")
 		want := "Hello, Javier"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
+
