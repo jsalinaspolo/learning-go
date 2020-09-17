f60e21b75f24cca86d573aad02368d2bdc6f5455
Javier Salinas <jsalinaspolo@gmail.com>
2020-09-17 23:41:07 +0200

    add sum slices

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,6 +1,7 @@
 package arrays_slices
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -20,4 +21,43 @@ func assertSum(t *testing.T, got int, want int, numbers []int) {
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("sum an empty", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("sum tails with only 1 element", func(t *testing.T) {
+		got := SumAllTails([]int{1}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
