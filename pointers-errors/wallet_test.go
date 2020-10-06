fbad0afd7f63d0cb62cef158cb908f5742fce863
Javier Salinas <jsalinaspolo@gmail.com>
2020-10-06 11:03:29 +0200

    Add pointers-errors section

---
+++ pointers-errors/wallet_test.go
@@ -0,0 +1,58 @@
+package pointers_errors
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, got Bitcoin, want Bitcoin) {
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+
+		assertBalance(t, got, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+
+		assertBalance(t, got, Bitcoin(10))
+		assertNoError(t, err)
+	})
+
+	assertError := func(t *testing.T, got error, want error) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
--- structs-interfaces/go.mod
+++ structs-interfaces/go.mod
@@ -1,3 +1,8 @@
-module iteration
+module structs_interfaces
 
 go 1.15
+
+require (
+	github.com/kisielk/errcheck v1.4.0 // indirect
+	golang.org/x/tools v0.0.0-20201005185003-576e169c3de7 // indirect
+)
